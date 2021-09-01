34e6c942b0c9ccb1ef5e9f1a9a23f6b2676c3b1a
claudio-guggis <claudio.gonzalez.g@usach.cl>
2021-09-01 03:48:56 -0400

    se  agrega estilo al rmulario de contacto y condiciones con las variables de sesión

--- formulariocontacto.php
+++ formulariocontacto.php
@@ -10,44 +10,108 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="assets/formulariocontacto.css">
     <title>Contacto</title>
 </head>
 <body>
-    <div class="contenedor-formulario">
+    <div class="contenedor-principal">
 
-        <div class="contenedor-logo">
-            <img src="imagenes/logo redondo.png" class="img-logo">
+        <div class="contenedor-izquierdo">
+            <div class="contenedor-logo">
+                <img src="imagenes/logo redondo.png" class="img-logo">
+            </div>
         </div>
 
-        <div class="label-formulario">
-            <label for="">¡Contáctate con nosotros!</label>
+        <div class="contenedor-derecho">
+
+                <div class="contenedor-formulario">
+                    <form action="enviar.php" class="form" method="POST">
+
+                        <div class="label-formulario">
+                            <label for="">¡Contáctate con nosotros!</label>
+                        </div>
+
+                        <div class="contenedor-nombres">
+                            <input class="input-nombres" type="text" placeholder="Nombre y apellidos" name="name">
+                        </div>
+
+                        <div class="contenedor-correo">
+                            <input class="input-correo" type="text" placeholder="Correo electrónico" name="email" required>
+                        </div>
+
+                        <div class="contenedor-telefono">
+                            <input class="input-telefono" type="text" placeholder="Teléfono" name="phone">
+                        </div>
+
+                        <div class="contenedor-areatexto">
+                            <textarea class="tacomentario" name="comment" id="comentario" cols="30" rows="10" placeholder="Comentario"></textarea>
+                        </div>
+
+                        <div class="contenedor-btn-enviar">
+                            <button class="btn-enviar" name="send">Enviar</button>
+                        </div>
+                    
+                    </form>
+
+                    <div class="contenedor-volver">
+                        <?php
+                            if(isset($_SESSION['type']))
+                            {
+                                $tipo = $_SESSION['type'];
+                                if($tipo == 'T')
+                                {
+                                    ?>
+                                    <button class="btn-volver" onclick="location.href='trabajador/misservicios.php'">Volver</button>
+                                    <?php
+                                }
+                                else
+                                {
+                                    ?>
+                                    <button class="btn-volver" onclick="location.href='cliente/cliente.php'">Volver</button>
+                                    <?php
+                                }
+                            }
+                            else
+                            {
+                                ?>
+                                <button class="btn-volver" onclick="location.href='login.php'">Volver</button>
+                                <?php
+                            }
+                        ?>
+                    </div>
+                    
+                </div>
+        
         </div>
 
-        <form action="enviar.php" class="form" method="POST">
+    </div>
 
-            <div class="contenedor-nombres">
-                <input type="text" placeholder="Nombre y apellidos" name="name">
-            </div>
-
-            <div class="contenedor-correo">
-                <input type="text" placeholder="Correo electrónico" name="email" required>
-            </div>
-
-            <div class="contenedor-telefono">
-                <input type="text" placeholder="Teléfono" name="phone">
-            </div>
-
-            <div>
-                <textarea name="comment" id="comentario" cols="30" rows="10" placeholder="Comentario"></textarea>
-            </div>
-
-            <button class="btn-enviar" name="send">Enviar</button>
-        </form>
-
-        <div class="contenedor-volver">
-            <a href="login.php"><button class="btn-volver">Volver</button></a>
-        </div>
+        <!-- <div class="contenedor-volver">
+            <?php
+                // if(isset($_SESSION['type']))
+                // {
+                //     $tipo = $_SESSION['type'];
+                //     if($tipo == 'T')
+                //    {
+                        ?>
+                        <a href="trabajador/misservicios.php"><button class="btn-volver">Volver</button></a>
+                        <?php
+                    // }
+                    // else
+                    // {
+                        ?>
+                        <a href="cliente/cliente.php"><button class="btn-volver">Volver</button></a>
+                        <?php
+                //     }
+                // }
+                // else
+                // {
+                    ?>
+                    <a href="login.php"><button class="btn-volver">Volver</button></a>
+                    <?php
+                // }
+            ?>
+        </div> -->
     </div>
 </body>
 </html>
