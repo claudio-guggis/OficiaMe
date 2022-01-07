b3995d38221f2a9614ee571b229b3ac12240a621
claudio-guggis <claudio.gonzalez.g@usach.cl>
2022-01-07 01:27:49 -0300

    se usan variables server

--- database.php
+++ database.php
@@ -1,15 +1,15 @@
 <?php
-    // $server = $_SERVER['database-ofme-prueba2.c6pqllnanjg0.sa-east-1.rds.amazonaws.com'];
-    // $username = $_SERVER['admin'];
-    // $password = $_SERVER['prueba1234'];
-    // $database = $_SERVER['prueba_ofme'];
-    // $port = $_SERVER['3306'];
+    $server = $_SERVER['database-ofme-prueba2.c6pqllnanjg0.sa-east-1.rds.amazonaws.com'];
+    $username = $_SERVER['admin'];
+    $password = $_SERVER['prueba1234'];
+    $database = $_SERVER['prueba_ofme'];
+    $port = $_SERVER['3306'];
 
-    $server = 'aa19t7qwob9kich.c6pqllnanjg0.sa-east-1.rds.amazonaws.com';
-    $username = 'admin';
-    $password = 'prueba1234';
-    $database = 'prueba_ofme';
-    $port = '3306';
+    // $server = 'aa19t7qwob9kich.c6pqllnanjg0.sa-east-1.rds.amazonaws.com';
+    // $username = 'admin';
+    // $password = 'prueba1234';
+    // $database = 'prueba_ofme';
+    // $port = '3306';
 
     //$link = new mysqli($server, $username, $password, $database, $port);
 
