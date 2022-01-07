3d642509214c66a4c91f7d377d14c324ac2417ae
claudio-guggis <claudio.gonzalez.g@usach.cl>
2022-01-06 23:21:47 -0300

    Se modifican parámetros en archivo database.php para la conexión a la base de datos aws rds

--- database.php
+++ database.php
@@ -1,9 +1,14 @@
 <?php
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'prueba_ofme';
+    $server = $_SERVER['database-ofme-prueba2.c6pqllnanjg0.sa-east-1.rds.amazonaws.com'];
+    $username = $_SERVER['admin'];
+    $password = $_SERVER['prueba1234'];
+    $database = $_SERVER['prueba_ofme'];
+    $port = $_SERVER['3306'];
+    $dbport = 3306;
 
-    $conexion = mysqli_connect($server, $username, $password, $database);
-    mysqli_set_charset($conexion, "utf8");
+    $link = new mysqli($server, $username, $password, $database, $port);
+
+    //$conexion = mysqli_connect($server, $username, $password, $database);
+    //mysqli_set_charset($conexion, "utf8");
+    mysqli_set_charset($link, "utf8");
 ?>
