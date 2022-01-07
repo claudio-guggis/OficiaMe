fea88b4d7db303d11e8d5faeee02ced427e962c3
claudio-guggis <claudio.gonzalez.g@usach.cl>
2022-01-07 01:36:04 -0300

    se vuelve a la variable conexion cambiando el nombre de database y agregando el port a la funcion connect

--- database.php
+++ database.php
@@ -1,19 +1,19 @@
 <?php
-    $server = $_SERVER['database-ofme-prueba2.c6pqllnanjg0.sa-east-1.rds.amazonaws.com'];
-    $username = $_SERVER['admin'];
-    $password = $_SERVER['prueba1234'];
-    $database = $_SERVER['pruebaofme'];
-    $port = $_SERVER['3306'];
+    // $server = $_SERVER['database-ofme-prueba2.c6pqllnanjg0.sa-east-1.rds.amazonaws.com'];
+    // $username = $_SERVER['admin'];
+    // $password = $_SERVER['prueba1234'];
+    // $database = $_SERVER['pruebaofme'];
+    // $port = $_SERVER['3306'];
 
-    // $server = 'aa19t7qwob9kich.c6pqllnanjg0.sa-east-1.rds.amazonaws.com';
-    // $username = 'admin';
-    // $password = 'prueba1234';
-    // $database = 'prueba_ofme';
-    // $port = '3306';
+    $server = 'aa19t7qwob9kich.c6pqllnanjg0.sa-east-1.rds.amazonaws.com';
+    $username = 'admin';
+    $password = 'prueba1234';
+    $database = 'pruebaofme';
+    $port = '3306';
 
-    $link = new mysqli($server, $username, $password, $database, $port);
+    //$link = new mysqli($server, $username, $password, $database, $port);
 
-    //$conexion = mysqli_connect($server, $username, $password, $database);
-    //mysqli_set_charset($conexion, "utf8");
-    mysqli_set_charset($link, "utf8");
+    $conexion = mysqli_connect($server, $username, $password, $database, $port);
+    mysqli_set_charset($conexion, "utf8");
+    //mysqli_set_charset($link, "utf8"); 
 ?>
