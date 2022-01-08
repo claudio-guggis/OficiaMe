71f5096c7beace2ed73e303e6678132787bb1310
claudio-guggis <claudio.gonzalez.g@usach.cl>
2022-01-08 17:19:02 -0300

    se vuelve a lo anterior

--- trabajador/responder.php
+++ trabajador/responder.php
@@ -2,9 +2,9 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     
-    require_once (__DIR__.'./../PHPMailer/Exception.php');
-    require_once (__DIR__.'./../PHPMailer/PHPMailer.php');
-    require_once (__DIR__.'./../PHPMailer/SMTP.php');
+    require ('./../PHPMailer/Exception.php');
+    require ('./../PHPMailer/PHPMailer.php');
+    require ('./../PHPMailer/SMTP.php');
 
     session_start();
 
